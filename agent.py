e30a770ad5a799c2aca500193215181625c87189
Jonas Laksen <jonaslaksen@live.com>
2017-10-19 23:08:07 +0200

    fsad

--- agent.py
+++ agent.py
@@ -1,5 +1,6 @@
 from keras.layers import Conv2D, Dropout, MaxPooling2D, Flatten, Dense
 from keras.models import Sequential, load_model
+from keras.optimizers import Adam
 
 
 class Agent:
@@ -8,20 +9,19 @@ class Agent:
             self.model = load_model('model.h5')
             return
         model = Sequential([
-            Conv2D(32, (5,5), input_shape=(160,320,3), activation='relu', data_format='channels_last'),
-            MaxPooling2D(),
-            Conv2D(32, (5,5), activation='relu'),
-            MaxPooling2D(),
-            Conv2D(64, (5,5), activation='relu'),
-            MaxPooling2D(),
-            Conv2D(128, (5,5), activation='relu'),
-            MaxPooling2D(),
+            Conv2D(32, (3,3), input_shape=(160,320,3), activation='relu', data_format='channels_last'),
+            MaxPooling2D(pool_size=(3,3)),
+            Conv2D(32, (3,3), activation='relu'),
+            MaxPooling2D(pool_size=(3,3)),
+            Conv2D(64, (3,3), activation='relu'),
+            MaxPooling2D(pool_size=(3,3)),
+            Conv2D(128, (3,3), activation='relu'),
             Flatten(),
             Dense(1024, activation='relu'),
             Dense(512, activation='relu'),
             Dense(1, activation='sigmoid')
         ])
-        model.compile(loss='mean_squared_error', optimizer='adam')
+        model.compile(loss='mean_squared_error', optimizer=Adam(lr=.1))
         self.model = model
 
     def train(self, X, y):
